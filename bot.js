1fff7fb54b2e688e0dfd6e9914c2530c774d654c
Fernando De los rios <fdelosrios@MacBook-Air-de-Fernando.local>
2026-04-07 23:14:33 +0200

    Read from BD SERVICIOS sheet

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ async function getSheetData() {
     const doc = new GoogleSpreadsheet(SHEET_ID);
     await doc.useServiceAccountAuth(credentials);
     await doc.loadInfo();
-    const sheet = doc.sheetsByIndex[0];
+    const sheet = doc.sheetsByTitle['BD SERVICIOS'];
     const rows = await sheet.getRows();
     return rows.map(row => ({
       fecha: row.get('FECHA') || row.get('Fecha'),
